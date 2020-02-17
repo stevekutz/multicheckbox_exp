534b80a0920dff804248385e16e90f101a4709df
stevekutz <stkutz@gmail.com>
2020-02-17 10:49:48 -0600

    added Segment & List semantic-UI-React

--- app/src/MultiCheck.js
+++ app/src/MultiCheck.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState} from 'reinspect';
-import {Input, List} from 'semantic-ui-react';
+import {Input, List, Segment} from 'semantic-ui-react';
 
 const style = {
     listContainer: {
@@ -34,16 +34,18 @@ function MultiCheck({options, onChange}) {
 
 
     return (
-        <ul style = {style.listContainer}>
-            {data.map(item => {
-                return (
-                    <li key = {item.label} style = {style.itemStyle} onClick = {()=> toggle(item)}>
-                        <Input  readOnly type = 'checkbox' checked = {item.checked || false }/>
-                        {item.label}
-                    </li>         
-                );                
-            })}       
-        </ul>
+        <Segment style = {{border: "1px solid red"}}>
+            <List style = {style.listContainer}>
+                {data.map(item => {
+                    return (
+                        <List.Item key = {item.label} style = {style.itemStyle} onClick = {()=> toggle(item)}>
+                            <Input  readOnly type = 'checkbox' checked = {item.checked || false }/>
+                            {item.label}
+                        </List.Item>         
+                    );                
+                })}       
+            </List>
+        </Segment>
     )
 
 };
